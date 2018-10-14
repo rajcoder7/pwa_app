d52443ed3f62400a7d8faf7c139928c496065553
rajcoder7 <rajcoder7@gmail.com>
2018-10-14 19:17:11 +0530

    Added code in pwa-sw.js

--- pwa-sw.js
+++ pwa-sw.js
@@ -1,4 +1,6 @@
+//This is the "Offline page" service worker
 
+//Install stage sets up the offline page in the cahche and opens a new cache
 self.addEventListener('install', function(event) {
   var offlinePage = new Request('offline.html');
   event.waitUntil(
@@ -10,6 +12,8 @@ self.addEventListener('install', function(event) {
   }));
 });
 
+//If any fetch fails, it will show the offline page.
+//Maybe this should be limited to HTML documents?
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).catch(function(error) {
@@ -20,6 +24,7 @@ self.addEventListener('fetch', function(event) {
     }));
 });
 
+//This is a event that can be fired from your page to tell the SW to update the offline page
 self.addEventListener('refreshOffline', function(response) {
   return caches.open('pwabuilder-offline').then(function(cache) {
     console.log('[PWA Builder] Offline page updated from refreshOffline event: '+ response.url);
